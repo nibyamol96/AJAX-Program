cb00a1ff31d70fa8a92fa346a06c14b607068e95
nibyamol96 <37066395+nibyamol96@users.noreply.github.com>
2019-11-27 16:03:00 +0530

    case study 2

---
+++ case3.js
@@ -0,0 +1,28 @@
+
+
+var xhr = new XMLHttpRequest();                 // Create XMLHttpRequest object
+
+xhr.onload = function() {                       // When readystate changes
+  // The following conditional check will not work locally - only on a server
+              // If server status was ok
+    responseObject = JSON.parse(xhr.responseText);
+
+    // BUILD UP STRING WITH NEW CONTENT (could also use DOM manipulation)
+    var newContent = '';
+    for (var i = 0; i < responseObject.events.length; i++) { // Loop through object
+      newContent += '<div class="event">';
+      newContent += '<img src="' + responseObject.events[i].map + '" ';
+      newContent += 'alt="' + responseObject.events[i].location + '" />';
+      newContent += '<p><b>' + responseObject.events[i].location + '</b><br>';
+      newContent += responseObject.events[i].date + '</p>';
+      newContent += '</div>';
+    }
+
+    // Update the page with the new content
+    document.getElementById('content').innerHTML = newContent;
+
+  //}
+};
+
+xhr.open('GET', 'case3.json', true);        // Prepare the request
+xhr.send(null);                                 // Send the request
